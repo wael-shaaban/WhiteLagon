a451bceca662b1ae54fe6a7401de68f4bf81fc5b
eleraky <shaabanw85@gmail.com>
2024-12-04 01:08:00 +0300

    Add project files.

---
+++ WhiteLagon.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WhiteLagon.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
